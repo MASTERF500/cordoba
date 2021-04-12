3f6249d28991d6e613032b2a869906da92009209
lenin <lenin.espinoza.alv@gmail.com>
2021-04-12 15:56:02 -0500

    BASE

--- BASE_API/server.js
+++ BASE_API/server.js
@@ -2,31 +2,27 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const User = require('./users')
+const Usuario = require('./usuarios')
 
-mongoose.connect('mongodb://localhost/pagination', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/refacc', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.once('open', async () => {
-  if (await User.countDocuments().exec() > 0) return
+  if (await Usuario.countDocuments().exec() > 0) return
 
   Promise.all([
-    User.create({ name: 'User 1' }),
-    User.create({ name: 'User 2' }),
-    User.create({ name: 'User 3' }),
-    User.create({ name: 'User 4' }),
-    User.create({ name: 'User 5' }),
-    User.create({ name: 'User 6' }),
-    User.create({ name: 'User 7' }),
-    User.create({ name: 'User 8' }),
-    User.create({ name: 'User 9' }),
-    User.create({ name: 'User 10' }),
-    User.create({ name: 'User 11' }),
-    User.create({ name: 'User 12' })
+    Usuario.create({ name: 'Edwin C.',email:'magnumf500@gmail.com',rol:'admin' }),
+    Usuario.create({ name: 'Edwin A.',email:'mag@gmail.com',rol:'admin' }),
   ]).then(() => console.log('Added Users'))
+})
+
+app.get('/usuarios', paginatedResults(Usuario), (req, res) => {
+  res.json(res.paginatedResults)
 })
 
 app.get('/users', paginatedResults(User), (req, res) => {
   res.json(res.paginatedResults)
 })
+
 
 function paginatedResults(model) {
   return async (req, res, next) => {
